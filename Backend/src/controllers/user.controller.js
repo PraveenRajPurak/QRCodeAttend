8665b0db5eadd4bd678adde05a512c4a06661dff
PraveenRajPurak <praveenrajpurak@gmail.com>
2024-09-16 12:47:16 +0530

    Error Handling 4

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -24,11 +24,18 @@ function haversine(lat1, lon1, lat2, lon2) {
     let Δφ = lat2 - lat1;
     let Δλ = lon2 - lon1;
 
+    console.log("Value of φ1: ", φ1);
+    console.log("Value of φ2: ", φ2);
+    console.log("Value of Δφ: ", Δφ);
+    console.log("Value of Δλ: ", Δλ);
+
     // Haversine formula
     let a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
             Math.cos(φ1) * Math.cos(φ2) *
             Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
     let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    console.log("Value of c: ", c);
 
     let distance = EARTH_RADIUS * c;
 
